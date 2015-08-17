358a54ffcd2b674ef0f0bda8de83928a0a1de888
Jeroen Ooms <jeroenooms@gmail.com>
2015-08-17 21:40:53 +0200

    Fix for r-devel

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,6 +1,6 @@
 # Build against static libraries from curl website.
 if(!file.exists("../windows/libssh2-1.6.0/include/libssh2.h")){
-  setInternet2()
+  if(getRversion() <= "3.2.2") setInternet2()
   download.file("https://github.com/rwinlib/libssh2/archive/v1.6.0.zip", "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
